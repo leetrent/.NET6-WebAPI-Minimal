9afcdc381b0d9f07fb160f012e4ef40d0548ddca
LeeTrent <lee.trent.1@gmail.com>
2022-04-14 11:10:12 -0500

    Added DTO to hide secret attribute in Model class

--- TodoApi/TodoApi/Program.cs
+++ TodoApi/TodoApi/Program.cs
@@ -7,39 +7,115 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello Casey & Pooh Bear!");
 
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// GET ALL
+//////////////////////////////////////////////////////////////////////////////////////////////
+//app.MapGet("/todoitems", async (TodoDb db) =>
+//    await db.Todos.ToListAsync());
+
 app.MapGet("/todoitems", async (TodoDb db) =>
-    await db.Todos.ToListAsync());
+    await db.Todos.Select(x => new TodoItemDTO(x)).ToListAsync());
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// GET ALL COMPLETED
+//////////////////////////////////////////////////////////////////////////////////////////////
+//app.MapGet("/todoitems/complete", async (TodoDb db) =>
+//    await db.Todos.Where(t => t.IsComplete).ToListAsync());
 
 app.MapGet("/todoitems/complete", async (TodoDb db) =>
-    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+    await db.Todos.Where(t => t.IsComplete).Select(x => new TodoItemDTO(x)).ToListAsync());
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// GET BY ID
+//////////////////////////////////////////////////////////////////////////////////////////////
+//app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+//    await db.Todos.FindAsync(id)
+//        is Todo todo
+//            ? Results.Ok(todo)
+//            : Results.NotFound());
 
 app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
     await db.Todos.FindAsync(id)
         is Todo todo
-            ? Results.Ok(todo)
+            ? Results.Ok(new TodoItemDTO(todo))
             : Results.NotFound());
 
-app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// POST
+//////////////////////////////////////////////////////////////////////////////////////////////
+//app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+//{
+//    db.Todos.Add(todo);
+//    await db.SaveChangesAsync();
+
+//    return Results.Created($"/todoitems/{todo.Id}", todo);
+//});
+
+app.MapPost("/todoitems", async (TodoItemDTO todoItemDTO, TodoDb db) =>
 {
-    db.Todos.Add(todo);
+    var todoItem = new Todo
+    {
+        IsComplete = todoItemDTO.IsComplete,
+        Name = todoItemDTO.Name
+    };
+
+    db.Todos.Add(todoItem);
     await db.SaveChangesAsync();
 
-    return Results.Created($"/todoitems/{todo.Id}", todo);
+    return Results.Created($"/todoitems/{todoItem.Id}", new TodoItemDTO(todoItem));
 });
 
-app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+//////////////////////////////////////////////////////////////////////////////////////////////
+// PUT
+//////////////////////////////////////////////////////////////////////////////////////////////
+//app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+//{
+//    var todo = await db.Todos.FindAsync(id);
+
+//    if (todo is null) return Results.NotFound();
+
+//    todo.Name = inputTodo.Name;
+//    todo.IsComplete = inputTodo.IsComplete;
+
+//    await db.SaveChangesAsync();
+
+//    return Results.NoContent();
+//});
+
+
+app.MapPut("/todoitems/{id}", async (int id, TodoItemDTO todoItemDTO, TodoDb db) =>
 {
     var todo = await db.Todos.FindAsync(id);
 
     if (todo is null) return Results.NotFound();
 
-    todo.Name = inputTodo.Name;
-    todo.IsComplete = inputTodo.IsComplete;
+    todo.Name = todoItemDTO.Name;
+    todo.IsComplete = todoItemDTO.IsComplete;
 
     await db.SaveChangesAsync();
 
     return Results.NoContent();
 });
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// DELETE
+//////////////////////////////////////////////////////////////////////////////////////////////
+//app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+//{
+//    if (await db.Todos.FindAsync(id) is Todo todo)
+//    {
+//        db.Todos.Remove(todo);
+//        await db.SaveChangesAsync();
+//        return Results.Ok(todo);
+//    }
+
+//    return Results.NotFound();
+//});
 
 app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
 {
@@ -47,12 +123,15 @@ app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
     {
         db.Todos.Remove(todo);
         await db.SaveChangesAsync();
-        return Results.Ok(todo);
+        return Results.Ok(new TodoItemDTO(todo));
     }
 
     return Results.NotFound();
 });
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+// RUN
+//////////////////////////////////////////////////////////////////////////////////////////////
 app.Run();
 
 class Todo
@@ -60,6 +139,7 @@ class Todo
     public int Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
+    public string? Secret { get; set; }
 }
 
 class TodoDb : DbContext
@@ -68,4 +148,15 @@ class TodoDb : DbContext
         : base(options) { }
 
     public DbSet<Todo> Todos => Set<Todo>();
+}
+
+class TodoItemDTO
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+
+    public TodoItemDTO() { }
+    public TodoItemDTO(Todo todoItem) =>
+    (Id, Name, IsComplete) = (todoItem.Id, todoItem.Name, todoItem.IsComplete);
 }
